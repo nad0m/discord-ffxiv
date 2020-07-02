6d354d2fc883b759d1d5d8811a8e0f6a5c96d65b
Adam Nguyen <adamdnguyen10@gmail.com>
2020-07-02 16:13:23 -0700

    add channel setter

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const client = new Discord.Client()
 const { getEmbed } = require('./embed')
 const { runSchedule } = require('./util')
 
+let channelId = '712206529981579355'
+const prefix = '!thorn'
+
 client.login(process.env.BOT_TOKEN);
 
 client.on("ready", () => {
@@ -14,7 +17,7 @@ client.on("ready", () => {
   });
 
   const onSchedule = () => {
-    const channel = client.channels.cache.get('712206529981579355')
+    const channel = client.channels.cache.get(channelId)
     onDataFinish = result => {
       channel.send({ embed: getEmbed(result) })
     }
@@ -27,11 +30,30 @@ client.on("message", msg => {
   const channel = client.channels.cache.get(msg.channel.id)
   const memberJoin = msg.type === "GUILD_MEMBER_JOIN";
 
-  if (msg.content === "!thorn status") {
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  const args = msg.content.slice(prefix.length).split(' ');
+  const command = args[1]
+
+  if (command === "set-channel") {
+    if (!args[2]) {
+      channel.send('Please provide a channel ID.')
+      return
+    }
+
+    channelId = args[2]
+    channel.send('Channel set. I will post automatic progress reports only to that channel.')
+  }
+
+  if (msg.content === "!thorn progress") {
     const cb = result => {
       channel.send({ embed: getEmbed(result) })
     }
     getStatus(cb)
+  }
+
+  if (msg.content.startsWith('!thorn setChannel')) {
+
   }
 
   if (memberJoin) {
